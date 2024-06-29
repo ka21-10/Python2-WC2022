0d582a6beff7cbafe719f9e54b9f72cb58b74df1
bbkhoi21 <phambaokhoi2005@gmail.com>
2024-06-29 22:22:20 +0700

    Update Python2_TamQuoc.py

--- Python2_TamQuoc.py
+++ Python2_TamQuoc.py
@@ -16,7 +16,7 @@ st.set_page_config(page_title="FIFA WC 2022 ANALYSIS",page_icon="⚽",layout="wi
 page_bg_img = """
         <style>
         [data-testid="stAppViewContainer"] > .main {
-            background-image: url('https://drive.google.com/file/d/1DeTxRmdIu0yfgiGILJOC-Jq5QieUw3u9/view?fbclid=IwZXh0bgNhZW0CMTAAAR1solKUGVMvVVoVH2GqmDXESCMKH75KuqOT-wsAVjzAgf5PgVicmqfC_Yk_aem_qjNumvJwQ3vAuZCjuuNCmg');
+            background-image: url("https://drive.google.com/file/d/1DeTxRmdIu0yfgiGILJOC-Jq5QieUw3u9/view?fbclid=IwZXh0bgNhZW0CMTAAAR1solKUGVMvVVoVH2GqmDXESCMKH75KuqOT-wsAVjzAgf5PgVicmqfC_Yk_aem_qjNumvJwQ3vAuZCjuuNCmg");
             background-size: 100%;
             background-position: center;
             background-repeat: no-repeat;
