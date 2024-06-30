0561c681c51c4ca5dc8bba0c027e57fb6bb9b6cb
ka21-10 <khoiphambao21@gmail.com>
2024-06-30 20:12:31 +0700

    Update Python2_TamQuoc.py

--- Python2_TamQuoc.py
+++ Python2_TamQuoc.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import re
-import seaborn as sns
+import seaborn as snsjjj
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 import plotly.express as px
